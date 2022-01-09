b56de30145c9115bdcb343261564251ca7e187a1
Watchdog <64688543+Watchdog069@users.noreply.github.com>
2022-01-09 19:06:46 +0530

    Update jkff.v

--- bin/jkff.v
+++ bin/jkff.v
@@ -5,31 +5,37 @@ module jkff(clk,j,k,rst,qnext,qbar);
 	always @(posedge clk)
 	begin 
 		if(rst) begin
-			qnext<=0;
-			qprev<=0;
+			qnext=0;
+			qbar= 1;
+			qprev=0;
 		end
+
 		else 
 		begin	
 			if(j==0 && k==0) begin
-				qnext<=qprev;
+				qnext=qprev;
 				qbar= !qnext;
-				qprev<=qnext; 
+				qprev=qnext; 
 			end
 			else if(j==0 && k==1) begin 
-				qnext<=0;
+				qnext=0;
 				qbar= !qnext;  
-				qprev<=qnext;
+				qprev=qnext;
 			end
 			else if(j==1 && k==0) begin
-				qnext<=1;
+				qnext=1;
 				qbar= !qnext; 
-				qprev<=qnext;		  	
+				qprev=qnext;		  	
 			end
-			else	begin		
-				qnext<= !qnext;
-				qbar= qnext;
+			else if(j==1 && k==1) begin		
+				qnext= !qnext;
+				qbar = !qnext;
 			end		
+			else begin
+				qnext=0;
+				qbar= 1;
+				qprev=0;
+			end
 		end			
-		
 	end
 endmodule 
